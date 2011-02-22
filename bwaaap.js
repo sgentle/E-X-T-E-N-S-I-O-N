f02c7b6568fee8cbba30887f7f7754bb88f0e4a8
Sam Gentle <sgentle@gmail.com>
2011-02-23 01:12:05 +1100

    initial commit

---
+++ bwaaap.js
@@ -0,0 +1,42 @@
+var matcher = /inception/i;
+var matchLength = 9; //I-N-C-E-P-T-I-O-N
+
+var bwaaap = new Audio(chrome.extension.getURL('bwaaap.mp3'));
+
+var nodeIterator = document.createNodeIterator(
+	document.body,
+	NodeFilter.SHOW_TEXT,
+	{ 
+		acceptNode : function (node) {
+			if (matcher.test(node.data)) {
+				return NodeFilter.FILTER_ACCEPT;
+			}
+			else {
+				return NodeFilter.FILTER_REJECT;
+			}
+	}},
+	true
+);
+
+var textNode;
+
+while ((textNode = nodeIterator.nextNode()) != null) {
+	var pos = textNode.data.search(matcher);
+	console.log("found at pos " + pos, textNode)	
+	window.matched = textNode;
+	//You know, in case the DOM has been modified so that it doesn't match
+	//in between the iterator testing and now. I laugh, but someone will do this.
+	if (pos > 0) {
+	    var secondNode = textNode.splitText(pos + matchLength);
+
+	    var img = document.createElement('img');
+		img.src = chrome.extension.getURL('bwaaap.png')
+		img.onclick = function() {
+			bwaaap.play();
+		}
+	
+	    textNode.parentNode.insertBefore(img, secondNode);
+
+		//The iterator should now hit secondNode
+	}
+}
